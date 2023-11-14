fce98427d7d81d520a01e5f6e081943169b6f03f
Henrique-Arau <penielh43@gmail.com>
2023-11-13 23:48:36 -0300

    Validando amigo já adicionado

--- js/app.js
+++ js/app.js
@@ -4,10 +4,16 @@ let amigos = [];
 function adicionar() {
 
     let amigo = document.getElementById('nome-amigo');
+
     if (amigo.value == '') {
         alert('Informe o nome do amigo!');
         return;
     }
+    if (amigos.includes(amigo.value)) {
+        alert('Nome já adicionado!');
+        return;
+    }
+
     let lista = document.getElementById('lista-amigos');
 
     amigos.push(amigo.value);
